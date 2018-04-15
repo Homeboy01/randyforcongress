27eb3124f2b20e7337fdddc48e8c8da826beafe8
Matthew Coleman <matthew@eudaimonia.io>
2018-04-15 16:45:37 -0400

    Define database values for Heroku

--- randyforcongress/heroku_settings.py
+++ randyforcongress/heroku_settings.py
@@ -1,3 +1,5 @@
+import os
+
 # This file is exec'd from settings.py, so it has access to and can
 # modify all the variables in settings.py.
 
@@ -12,13 +14,13 @@ DATABASES = {
         # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
         "ENGINE": "django.db.backends.postgresql",
         # DB name or path to database file if using sqlite3.
-        "NAME": "postgres",
+        "NAME": os.environ['DATABASE_NAME'],
         # Not used with sqlite3.
-        "USER": "postgres",
+        "USER": os.environ['DATABASE_USER'],
         # Not used with sqlite3.
-        "PASSWORD": "randyforcongress",
+        "PASSWORD": os.environ['DATABASE_PASSWORD'],
         # Set to empty string for localhost. Not used with sqlite3.
-        "HOST": "db",
+        "HOST": os.environ['DATABASE_URL'],
         # Set to empty string for default. Not used with sqlite3.
         "PORT": "5432",
     }
