38c94bd69fdccaabd8c898768a4a816fd7ed08d4
Matthew Coleman <matthew@eudaimonia.io>
2018-04-16 11:53:53 -0400

    Fix an environment value call

--- randyforcongress/heroku_settings.py
+++ randyforcongress/heroku_settings.py
@@ -5,7 +5,7 @@ import os
 # AWS SETTINGS #
 ################
 
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_SECRET_KEY_ID']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = 'randyforcongress'
